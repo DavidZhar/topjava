0aff7e3a0bf880d7ff7a13e033a982226e7ba3ef
DavidZhar <10191518tata>
2020-06-06 19:05:52 +0300

    hw01 base

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -19,15 +19,15 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(UserServlet.class);
 
-    public static List<MealTo> meals = Arrays.asList(
-            new MealTo(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500, false),
-            new MealTo(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000, false),
-            new MealTo(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500, false),
-            new MealTo(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100, true),
-            new MealTo(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000, true),
-            new MealTo(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500, true),
-            new MealTo(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410, true)
-    );
+//    public static List<MealTo> meals = Arrays.asList(
+//            new MealTo(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500, false),
+//            new MealTo(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000, false),
+//            new MealTo(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500, false),
+//            new MealTo(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100, true),
+//            new MealTo(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000, true),
+//            new MealTo(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500, true),
+//            new MealTo(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410, true)
+//    );
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -37,15 +37,15 @@ public class MealServlet extends HttpServlet {
         log.debug("redirect to meals");
 
         List<MealTo> meals = Arrays.asList(
-                new MealTo(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500, false),
-                new MealTo(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000, false),
-                new MealTo(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500, false),
+                new MealTo(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Креветка", 500, false),
+                new MealTo(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Краб", 1000, true),
+                new MealTo(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Угорь", 500, false),
                 new MealTo(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100, true),
                 new MealTo(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000, true),
-                new MealTo(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500, true),
+                new MealTo(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500, false),
                 new MealTo(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410, true)
         );
-        request.setAttribute("meals", meals.get(1).toString());
+        request.setAttribute("meals", meals);
 
         request.getRequestDispatcher("/meals.jsp").forward(request, response);
 //        response.sendRedirect("meals.jsp");
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,8 +1,6 @@
-<%@ page import="java.util.List" %>
-<%@ page import="ru.javawebinar.topjava.model.MealTo" %>
-<%@ page import="java.io.IOException" %>
-<%@ page import="ru.javawebinar.topjava.web.MealServlet" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib uri="http://sargue.net/jsptags/time" prefix="javatime" %>
 
 <%--
   Created by IntelliJ IDEA.
@@ -11,35 +9,44 @@
   Time: 20:27
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>Meals</title>
+    <style>
+        table {
+            border-collapse: separate; /* Способ отображения границы */
+            width: 100%; /* Ширина таблицы */
+        }
+        td {
+            padding: 12px; /* Поля вокруг текста */
+            text-align: left;
+        }
+        tr:nth-child(even) {background-color: #f2f2f2;}
+    </style>
 </head>
 <body>
-<% response.getWriter().println("test1<br>"); %>
-test2 <br>
-<%="test3<br>" %>
-<%=MealServlet.meals.get(0).toString() %>
+<h2><a href="/topjava">Home</a></h2>
+<h3 align="center">MEALS</h3>
 <br>
-<%=(String) request.getAttribute("meals") %>
-
-<p><%
-
-
-%></p>
-
-<%--<%--%>
-<%--    List<MealTo> meals = (List<MealTo>) request.getAttribute("meals");--%>
-<%--    MealServlet.meals.forEach(meal -> {--%>
-<%--        try {--%>
-<%--            out.(meal.toString());--%>
-<%--            out.--%>
-<%--        } catch (IOException e) {--%>
-<%--            e.printStackTrace();--%>
-<%--        }--%>
-<%--    }); %>--%>
-
-
+<table align="center">
+    <tr align="left">
+        <th style="padding-left: 12px; padding-top: 12px; padding-bottom: 12px; background-color: forestgreen; color: white;">Date and time</th>
+        <th style="padding-left: 12px; padding-top: 12px; padding-bottom: 12px; background-color: forestgreen; color: white;">Description</th>
+        <th style="padding-left: 12px; padding-top: 12px; padding-bottom: 12px; background-color: forestgreen; color: white;">Calories</th>
+    </tr>
+<c:forEach var="meal" items="${meals}">
+    <c:if test="${!meal.excess}">
+        <tr style="color: red;">
+    </c:if>
+    <c:if test="${meal.excess}">
+        <tr style="color: green;">
+    </c:if>
+    <javatime:format pattern="dd-MM-yyyy HH:mm" value="${meal.dateTime}" var="date"/>
+    <td>${date}</td>
+        <td>${meal.description}</td>
+        <td>${meal.calories}</td>
+    </tr>
+</c:forEach>
+</table>
 </body>
 </html>
